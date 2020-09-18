68f1a8b918842812174f9a3829cc843b0e2f38bb
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-09-18 10:04:43 -0400

    started WorkTicket class

---
+++ OOP3200-Lab1/WorkTicket.cpp
@@ -0,0 +1,16 @@
+/* Program Name :
+* Authors : Ryan Claysonand Daniel Hinbest
+* Date :
+* Description :
+*/
+class WorkTicket
+{
+private:
+	int ticketNumber;
+	int clientID;
+	int ticketDay;
+	int ticketMonth;
+	int ticketYear;
+public:
+	int issueDescription;
+};
